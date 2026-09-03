740fab5f67f42667f9d2a66c24ffcff621031c28
Sitemaker2302 <unrujack@gmail.com>
2026-09-03 16:51:08 +0900

    Create AccountantRegister.jsx

---
+++ src/pages/AccountantRegister.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PortalRegister from "@/components/site/PortalRegister";
+
+export default function AccountantRegister() {
+  return <PortalRegister kind="accountant" />;
+}
