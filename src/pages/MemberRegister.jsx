7d749dccceb0ebeab6b04a98038bb68ae447c010
Sitemaker2302 <unrujack@gmail.com>
2026-09-03 16:55:58 +0900

    Create MemberRegister.jsx

---
+++ src/pages/MemberRegister.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PortalRegister from "@/components/site/PortalRegister";
+
+export default function MemberRegister() {
+  return <PortalRegister kind="member" />;
+}
